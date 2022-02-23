5e4873398b2917dbb926e15db8c4a6a82567c057
Giuseppe Polverino <giuseppe.polverino@dedagroup.it>
2022-02-23 12:35:53 +0100

    replace old api

--- js/main.js
+++ js/main.js
@@ -17,18 +17,21 @@ App.IndexController = Ember.Controller.extend({
     ajaxCall: null,
     urlCount: 0,
     peoplePageNumer: 1,
-    startShipPageNumer: 4,
+    startShipPageNumber: 3,
     buttonText: 'Start Game',
     userWinCount: 0,
     startShipWinCount: 0,
     // load people from swapi
     loadPeople(callback) {
         var that = this;
-        $.ajax('https://swapi.co/api/people/.json?page=' + that.get('peoplePageNumer'))
+        $.ajax('http://swapi.dev/api/people/' + that.get('peoplePageNumer'))
             .then(function (response) {
-                that.set("userlist", response.results)
+                that.set("userlist", response)
+                if (response.starships) {
+                    that.starShipUrl = response.starships;
+                }
                 // fix unknown mass
-                var fixMass = response.results;
+                var fixMass = response;
                 Array.prototype.removeValue = function(name, value){
                    var array = $.map(this, function(v,i){
                       return v[name] === value ? null : v;
@@ -36,10 +39,10 @@ App.IndexController = Ember.Controller.extend({
                    this.length = 0; 
                    this.push.apply(this, array);
                 }
-                fixMass.removeValue('mass', 'unknown');
+               // fixMass.removeValue('mass', 'unknown');
 
-                var randomNumber = Math.floor(Math.random() * response.results.length);
-                that.set('currentUser', response.results[randomNumber])
+                var randomNumber = Math.floor(Math.random() * response.length);
+                that.set('currentUser', response[randomNumber])
 
 
                 var nextPage = response.count - (that.get('peoplePageNumer') * 10);
@@ -54,10 +57,18 @@ App.IndexController = Ember.Controller.extend({
     // load starship from swapi
     loadStarShip(callback) {
         var that = this;
-        $.ajax('https://swapi.co/api/starships/.json?page=' + that.get('startShipPageNumer')).then(function (response) {
-            that.set("startshipList", response.results);
+        let starShipPage;
+        let formatPage;
+        if (that.starShipUrl && that.starShipUrl.length > 0) {
+            starShipPage = that.starShipUrl[0].split('/');
+            formatPage = starShipPage[starShipPage.length - 2];
+        } else {
+            formatPage = 2;
+        }
+        $.ajax('http://swapi.dev/api/starships/' + formatPage).then(function (response) {
+            that.set("startshipList", response);
             // fix unknown crew
-            var fixCrew = response.results;
+            var fixCrew = response;
             Array.prototype.removeValue = function(name, value){
                var array = $.map(this, function(v,i){
                   return v[name] === value ? null : v;
@@ -65,15 +76,15 @@ App.IndexController = Ember.Controller.extend({
                this.length = 0; 
                this.push.apply(this, array);
             }
-            fixCrew.removeValue('crew', 'unknown');
+            // fixCrew.removeValue('crew', 'unknown');
 
-            var randomNumber = Math.floor(Math.random() * response.results.length);
-            that.set('currentStartship', response.results[randomNumber])
-            var nextPage = response.count - (that.get('startShipPageNumer') * 10);
+            var randomNumber = Math.floor(Math.random() * response.length);
+            that.set('currentStartship', response[randomNumber])
+            var nextPage = response.count - (that.get('startShipPageNumber') * 10);
             if (nextPage <= 10) {
-                that.set('startShipPageNumer', 1)
+                that.set('startShipPageNumber', 1)
             } else {
-                that.set('startShipPageNumer', that.get('startShipPageNumer') + 1)
+                that.set('startShipPageNumber', that.get('startShipPageNumber') + 1)
             }
             callback();
         })
@@ -91,16 +102,8 @@ App.IndexController = Ember.Controller.extend({
     startGame() {
         var that = this;
         that.set('loadingBar', true)
-        if (that.urlCount == 0 || that.urlCount == 10) {
-            that.set('urlCount', 1);
-            that.loadData();
-        } else {
-            that.set('urlCount', that.urlCount + 1);
-            that.set('currentUser', that.get('userlist')[Math.floor(Math.random() * that.get('userlist').length)])
-            that.set('currentStartship', that.get('startshipList')[Math.floor(Math.random() * that.get('startshipList').length)])
-            that.checkWinner();
-        }
-
+        that.set('urlCount', 1);
+        that.loadData();
     },
     // check winner
     checkWinner() {
@@ -108,8 +111,8 @@ App.IndexController = Ember.Controller.extend({
         var winner = {}
         var userWinCount = that.get('userWinCount')
         var startShipWinCount = that.get('startShipWinCount')
-        if (parseInt(that.get('currentUser').mass) && parseInt(that.get('currentStartship').crew)) {
-            if (parseInt(that.get('currentUser').mass) > parseInt(that.get('currentStartship').crew)) {
+        if (parseInt(that.get('userlist').mass) && parseInt(that.get('startshipList').crew)) {
+            if (parseInt(that.get('userlist').mass) > parseInt(that.get('startshipList').crew)) {
                 winner.message = 'Player 1 win the game';
 
                 winner.chip = 'player1';
